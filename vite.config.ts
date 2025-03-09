48c3e63ae72cb13e91e9ecacc0ce49fac1980848
Taxmasib <taxmasib78@gmail.com>
2025-03-09 23:54:38 +0400

    Fix paths and build configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,28 +5,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TonBakuHub/',
+  base: "/TonBakuHub",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          const name = assetInfo.name || '';
-          const info = name.split('.');
-          const ext = info[info.length - 1];
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
-            return `[name][extname]`;
-          }
-          return `assets/[name][extname]`;
-        },
-        chunkFileNames: 'assets/[name].js',
-        entryFileNames: 'assets/[name].js',
-      },
     },
   },
 });
