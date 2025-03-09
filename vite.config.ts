922a0b1fcd565d6ac54ae2354e1a8dc0dfee8840
Taxmasib <taxmasib78@gmail.com>
2025-03-09 23:29:27 +0400

    ix paths and build configuration for itub ages

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,28 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/TonBakuHub/' : '/',
+  base: '/TonBakuHub/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const name = assetInfo.name || '';
+          const info = name.split('.');
+          const ext = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `[name][extname]`;
+          }
+          return `assets/[name][extname]`;
+        },
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js',
+      },
     },
   },
 });
