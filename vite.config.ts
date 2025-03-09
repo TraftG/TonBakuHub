61e4e981b8f5c5be3e4b7af989bad7f286fbcc59
Taxmasib <taxmasib78@gmail.com>
2025-03-09 23:01:38 +0400

    ix paths for development and production

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TonBakuHub/',
+  base: process.env.NODE_ENV === 'production' ? '/TonBakuHub/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
